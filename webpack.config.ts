ffeec40ca231a1c5bb07d6d8bfd0121725fded06
Fred <lemonpaimc@126.com>
2021-08-03 19:47:49 +0800

    ci(webpack): webpack静态资源路径问题

--- webpack.config.ts
+++ webpack.config.ts
@@ -7,17 +7,17 @@ import { config } from "./config/index";
 const isProduction = process.env.NODE_ENV == "production";
 
 env({
-	path: join(__dirname, `${process.env.NODE_ENV}.env`),
+  path: join(__dirname, `${process.env.NODE_ENV}.env`),
 });
 
 if (isProduction) {
-	config.mode = "production";
+  config.mode = "production";
+  config.output.publicPath = "/auth/";
+  config.plugins.push(new MiniCssExtractPlugin());
 
-	config.plugins.push(new MiniCssExtractPlugin());
-
-	config.plugins.push(new WorkboxWebpackPlugin.GenerateSW());
+  config.plugins.push(new WorkboxWebpackPlugin.GenerateSW());
 } else {
-	config.mode = "development";
+  config.mode = "development";
 }
 
 export default config;
