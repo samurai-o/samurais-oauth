2bcc8e6b2f24073c84997e2945c45e8412499946
Fred <lemonpaimc@126.com>
2021-08-04 06:23:30 +0800

    fix: 打包问题修复

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,13 @@ import ReactDOM from "react-dom";
 import * as Sentry from "@sentry/react";
 import { Integrations } from "@sentry/tracing";
 import { Main } from "./router";
-// const { NODE_ENV, APPVERSION } = process.env;
+const { NODE_ENV, APPVERSION } = process.env;
 
-// Sentry.init({
-// 	dsn: "https://b3280df6c5ba4a8aa2a8ef95eafa4588@o937351.ingest.sentry.io/5887815",
-// 	integrations: [new Integrations.BrowserTracing()],
-// 	release: `${NODE_ENV}@${APPVERSION}`,
-// 	tracesSampleRate: 1.0,
-// });
+Sentry.init({
+	dsn: "https://b3280df6c5ba4a8aa2a8ef95eafa4588@o937351.ingest.sentry.io/5887815",
+	integrations: [new Integrations.BrowserTracing()],
+	release: `${NODE_ENV}@${APPVERSION}`,
+	tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(<Main />, document.getElementById("app"));
