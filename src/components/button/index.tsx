c83c3a86f5a2a809e311b1c7d5fa03d45741bf88
刘伟 <lemonpaimc@126.com>
2021-08-29 07:58:55 +0800

    refactor: input组件功能优化:前后缀处理

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -14,7 +14,7 @@ export type ButtonProps = {
 	disabled?: boolean;
 	loading?: boolean;
 	children?: string | JSX.Element;
-	onClick?: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+	onClick?: (e: React.MouseEvent<HTMLButtonElement | HTMLAnchorElement, MouseEvent>) => void;
 };
 
 /**
@@ -24,15 +24,14 @@ export type ButtonProps = {
  */
 export function Button(props: ButtonProps): JSX.Element {
 	const { htmlType = "button", pattern = "default", children, loading = false } = props;
-
-	if (pattern === "link") return <LinkElement>{children}</LinkElement>;
-
-
-	const _onClick = useCallback((e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+	const _onClick = useCallback((e: React.MouseEvent<HTMLButtonElement | HTMLAnchorElement, MouseEvent>) => {
+		if (props.loading) return;
 		// 禁止默认事件
 		if (props.disabled) e.preventDefault();
 		if (isFunc(props.onClick) && !props.disabled) props.onClick(e);
 	}, [props]);
+
+	if (pattern === "link") return <LinkElement onClick={_onClick}>{children}</LinkElement>;
 
 	return (
 		<ButtonElement type={htmlType} pattern={pattern} onClick={_onClick}>
